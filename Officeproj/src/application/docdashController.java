70ee3969cea0137930678df8d782d2236be83c63
Avi Aaron Batchu <aviaaron2003@gmail.com>
2024-07-06 17:37:29 -0500

    functionality integration v1

---
+++ Officeproj/src/application/docdashController.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class docdashController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	
+	public void switchToRolePicker(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Rolepicker.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+	}
+	
+	public void switchToPatientMessages(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("DocMessages.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+	}
+	
+	public void switchToPatientLookup(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Lookup.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+	}
+}
